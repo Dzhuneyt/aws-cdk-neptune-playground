2ccfe59c1da50b16d02d89ad1542936b75471e22
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2023-06-27 17:00:03 +0300

    Initial commit

---
+++ test/neptune-quickstart.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as NeptuneQuickstart from '../lib/neptune-quickstart-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/neptune-quickstart-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new NeptuneQuickstart.NeptuneQuickstartStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
